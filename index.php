a36f62d1a0fe7492a4c61f69db57914449725285
agomez95 <agomez95@adriangv95.local>
2024-01-11 18:21:19 -0500

    paginacion v4

--- index.php
+++ index.php
@@ -23,9 +23,60 @@
 
     <link href="./assets/images/ICONOS/Favicon postgrado 1.webp" rel="icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
 
-    <!-- <link rel="stylesheet" href="./assets/css/main.css"> -->
+        <!-- For favicon png -->
+        <link rel="shortcut icon" type="image/icon" href="assets/logo/favicon.png" />
+
+    <!--font-awesome.min.css-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <!--linear icon css-->
+    <link rel="stylesheet" href="assets/css/linearicons.css">
+
+    <!--slick.css-->
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/slick-theme.css">
+
+    <!--bootstrap.min.css-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!-- bootsnav -->
+    <link rel="stylesheet" href="assets/css/bootsnav.css">
+
+    <!--style.css-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!--responsive.css-->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <!--videotutorial.css-->
+    <link rel="stylesheet" href="assets/css/videotutorial.css">
+
+    <!--animate.css-->
+    <link rel="stylesheet" href="assets/css/animate.css">
+
+    <!--flaticon.css-->
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+
+    <!--slick.css-->
+    <link rel="stylesheet" href="assets/css/slick.css">
+
+    <link rel="stylesheet" href="assets/css/slick-theme.css">
+
+    <!--bootstrap.min.css-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!-- bootsnav -->
+    <link rel="stylesheet" href="assets/css/bootsnav.css">
+
+    <!--style.css-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!--responsive.css-->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <!--videotutoriales.css-->
+    <link rel="stylesheet" href="assets/css/videotutoriales.css">
 
 </head>
 
@@ -52,8 +103,139 @@
     include("./paginas/footer.php");
     ?>
 
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
+    <script>
+        $('#myCarousel').carousel({
+            interval: 4000
+        });
+    </script>
 
+    <script type="text/javascript">
+        window.onload = function() {
+            const options = document.querySelectorAll('.tab-nosotros .option');
+
+            mostrarDefaultInfo(options[0]);
+
+            options[0].classList.add('selected');
+
+            options.forEach(option => {
+                option.addEventListener('click', function() {
+                    options.forEach(opt => {
+                        opt.classList.remove('selected');
+                    });
+
+                    this.classList.add('selected');
+
+                    const optionId = this.id;
+                    if (optionId === 'option4') {
+                        mostrarContenidoEspecial();
+                    } else {
+                        mostrarInformacion(optionId);
+                    }
+                });
+            });
+        };
+
+        function mostrarDefaultInfo(option) {
+            mostrarInformacion(option.id);
+        }
+
+        function mostrarInformacion(optionId) {
+            const insideDiv = document.querySelector('.inside');
+            insideDiv.innerHTML = '';
+
+            let title = '';
+            let content = '';
+
+            switch (optionId) {
+                case 'option1':
+                    title = '¿QUÉ HACEMOS?';
+                    content = 'La Oficina de Educación Virtual de la Universidad Privada de Tacna, asume funciones relacionadas con la educación virtual y da soporte a la enseñanza de pregrado, postgrado y CPU con las Aulas Virtuales y Sistema de Videoconferencias.' +
+                        'La Universidad Privada de Tacna, con enorme esfuerzo académico y financiero ha puesto en servicio una eficiente plataforma electrónica que está optimizando las acciones educativas brindadas por la universidad, en cumplimiento de sus aspiraciones de ofrecer a los estudiantes y a la comunidad, los beneficios de la aplicación de los avances de las TICs con fines educativos y culturales, de acuerdo con los valores asumidos como visión y misión de esta casa superior de estudios.';
+                    break;
+                case 'option2':
+                    title = 'MISIÓN';
+                    content = 'La Oficina de Educación Virtual, es un órgano de apoyo técnico-normativo, encargado de organizar, normar, coordinar y supervisar la ejecución de los programas de educación a distancia, propuestos por las facultades y escuela de Postgrado, para la comunidad universitaria y población en general. Es el órgano de apoyo encargado de la administración y mantenimiento del sistema de educación virtual que incluye aula virtual, sistema de videoconferencia y soporte informático de aplicaciones orientadas a cubrir las necesidades de la educación no presencial de la Universidad Privada de Tacna Y depende del Vicerrectorado Académico.';
+                    break;
+                case 'option3':
+                    title = 'VISIÓN';
+                    content = 'Ser una oficina de apoyo académico logrando el posicionamiento y liderazgo en formación virtual y semipresencial en la Universidad Privada de Tacna; brindado asesoría tecnológica y orientación pedagógica-didáctica en forma oportuna y con calidad, e incorporando la creación, la innovación para el mejoramiento en los procesos académicos.';
+                    break;
+                default:
+                    title = '';
+                    content = '';
+                    break;
+            }
+
+            const contentDiv = document.createElement('div');
+            contentDiv.classList.add('content');
+
+            const h2 = document.createElement('h2');
+            h2.textContent = title;
+            h2.classList.add('titulo-nosotros');
+
+            const p = document.createElement('p');
+            p.textContent = content;
+            p.classList.add('parrafo-nosotros');
+
+            contentDiv.appendChild(h2);
+            contentDiv.appendChild(p);
+
+            insideDiv.appendChild(contentDiv);
+        }
+
+        function mostrarContenidoEspecial() {
+            const contenidoEspecial = document.getElementById('contenidoEspecial').innerHTML;
+
+            let title = '';
+            let content = '';
+
+            title = 'SOPORTE TÉCNICO DEL AULA VIRTUAL';
+            content = 'La Coordinación Académica de Aulas Virtuales proporciona asistencia y soporte técnico al docente y estudiante durante el desarrollo de las clases programadas en el semestre académico, con el fin de atender sus consultas y/o requerimientos.';
+
+            const contentDiv = document.createElement('div');
+            contentDiv.classList.add('content');
+
+            const h2 = document.createElement('h2');
+            h2.textContent = title;
+            h2.classList.add('titulo-nosotros');
+
+            const p = document.createElement('p');
+            p.textContent = content;
+            p.classList.add('parrafo-nosotros');
+
+            contentDiv.appendChild(h2);
+            contentDiv.appendChild(p);
+
+            const insideDiv = document.querySelector('.inside');
+            insideDiv.innerHTML = contenidoEspecial;
+        }
+    </script>
+
+    <script src="assets/js/jquery.js"></script>
+
+    <!--modernizr.min.js-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+
+    <!--bootstrap.min.js-->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- bootsnav js -->
+    <script src="assets/js/bootsnav.js"></script>
+
+    <!--feather.min.js-->
+    <script src="assets/js/feather.min.js"></script>
+
+    <!-- counter js -->
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+
+    <!--slick.min.js-->
+    <script src="assets/js/slick.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+
+    <!--Custom JS-->
+    <script src="assets/js/custom.js"></script>
 </body>
 
 </html>
